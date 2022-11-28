fe3242be7b1a53c5d3c0dee7d3e9fa8afbb32b9b
PanJohnny <devstefancz@gmail.com>
2022-11-28 22:48:28 +0100

    redone lot of stuff

--- src/pages/api/feed.ts
+++ src/pages/api/feed.ts
@@ -2,7 +2,9 @@ import { XMLParser } from "fast-xml-parser";
 const parser = new XMLParser();
 
 export async function get({ params }) {
-  return new Response(JSON.stringify(await getFeed(false)), {
+  console.log(params);
+
+  return new Response(JSON.stringify(await getFeed()), {
     status: 200,
     headers: {
       "Content-Type": "application/json",
@@ -10,8 +12,33 @@ export async function get({ params }) {
     }
   });
 }
+
+export enum Category {
+  Zadna = "",
+  Novinky = "novinky",
+  Uspechy = "uspechy-skoly"
+}
+
 // rss > channel > [item
-export async function getFeed(novinky?:boolean) {
-  const content = await fetch(novinky?"https://www.gymlit.cz/category/novinky/feed/":"https://www.gymlit.cz/feed/").then((data) => data.text());
+export async function getFeed(category?: Category, page?: number) {
+  if (!page)
+    page = 0;
+  if (!category)
+    category = Category.Zadna
+
+  // first try
+  let categoryString: string = "/category/" + Category[category];
+
+  // magic black wizardry shit
+  if (Object.values(Category).includes(category))
+    categoryString = "/category/" + category;
+
+  if (category == Category.Zadna || category + "" == "Zadna") {
+    categoryString = "";
+  }
+  const url = "https://www.gymlit.cz" + categoryString + "/feed/" + "?paged=" + page
+
+  const content = await fetch(url).then((data) => data.text());
+
   return parser.parse(content).rss.channel.item;
 }
